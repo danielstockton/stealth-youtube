377d14e16e82cd101c51c048c7654e0ee6e42c29
Daniel Stockton <me@danielstockton.com>
2014-10-19 01:47:41 +0300

    Remove thumbnails and reduce font size of titles.

--- stealth-youtube.js
+++ stealth-youtube.js
@@ -1,2 +1,24 @@
-document.getElementsByClassName("html5-video-container")[0].style.display="none";
-document.getElementById("player-api").style.height="30px";
+// Remove video.
+document.getElementsByClassName("html5-video-container")[0].style.display = "none";
+document.getElementById("player-api").style.height = "30px";
+
+// Remove thumbnails.
+var thumbnails = document.getElementsByClassName("yt-uix-simple-thumb-wrap");
+var videoThumbnails = document.getElementsByClassName("video-thumb");
+
+for (var i = 0, thumbnail; thumbnail = thumbnails[i]; i++) {
+  thumbnail.style.display = "none";
+}
+
+for (var i = 0, thumbnail; thumbnail = videoThumbnails[i]; i++) {
+  thumbnail.style.display = "none";
+}
+
+// Reduce font size of titles.
+document.getElementById('eow-title').style.fontSize = "10px";
+var titles = document.getElementsByClassName("title");
+
+for (var i = 0, title; title = titles[i]; i++) {
+  title.style.fontSize = "10px";
+}
+
