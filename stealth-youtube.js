147c908bf6b347277bb89dd50518fd91d43088a9
Daniel Stockton <me@danielstockton.com>
2014-10-19 11:03:42 +0300

    Refactor javascript.

--- stealth-youtube.js
+++ stealth-youtube.js
@@ -3,20 +3,14 @@ document.getElementsByClassName("html5-video-container")[0].style.display = "non
 document.getElementById("player-api").style.height = "30px";
 
 // Remove thumbnails.
-var thumbnails = document.getElementsByClassName("yt-uix-simple-thumb-wrap");
-var videoThumbnails = document.getElementsByClassName("video-thumb");
+var thumbnails = document.querySelectorAll(".yt-uix-simple-thumb-wrap, .video-thumb");
 
 for (var i = 0, thumbnail; thumbnail = thumbnails[i]; i++) {
   thumbnail.style.display = "none";
 }
 
-for (var i = 0, thumbnail; thumbnail = videoThumbnails[i]; i++) {
-  thumbnail.style.display = "none";
-}
-
 // Reduce font size of titles.
-document.getElementById('eow-title').style.fontSize = "10px";
-var titles = document.getElementsByClassName("title");
+var titles = document.querySelectorAll("#eow-title, .title");
 
 for (var i = 0, title; title = titles[i]; i++) {
   title.style.fontSize = "10px";
