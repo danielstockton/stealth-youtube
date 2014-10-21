fabaa2ac3e16c9dcc521384aaba9e013d1d4bf0d
Mihnea Simian <contact@mesimian.com>
2014-10-21 18:15:40 +0300

    now hiding thumbnails, smaller font size in titles still trying to make thumbnail work on song change

--- stealth-youtube.js
+++ stealth-youtube.js
@@ -1,2 +1,23 @@
-document.getElementsByClassName("html5-video-container")[0].style.display="none";
-document.getElementById("player-api").style.height="30px";
+var obstructView = function () {
+    if (!document.getElementsByClassName("html5-video-container").length) {
+        return;
+    }
+    document.getElementsByClassName("html5-video-container")[0].style.display="none";
+    document.getElementById("player-api").style.height="30px";
+
+    document.getElementById("eow-title").style.fontSize = "10px";
+    (function obstructRelated() {
+        var thumbnails = document.getElementsByClassName("yt-uix-simple-thumb-wrap"),
+            titles = document.getElementById("watch-related").getElementsByClassName("title");
+        for (var thumbInd = 0; thumbInd < thumbnails.length; thumbInd++) {
+            thumbnails[thumbInd].style.display = "none";
+        }
+        for (var titleInd = 0; titleInd < titles.length; titleInd++) {
+            titles[titleInd].style.fontSize = "10px";
+        }
+    })();
+};
+
+obstructView();
+
+document.getElementsByTagName("body").onclick = obstructView;
