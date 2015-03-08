2839a885f2305033f5bed496e66483adb842fbde
Mihnea Simian <contact@mesimian.com>
2015-03-08 12:29:10 +0200

    popup settings now using new settings obj in storage

--- popup.js
+++ popup.js
@@ -1,9 +1,13 @@
-chrome.storage.sync.get('enabled', function(storageValues) {
-  $('#enabled').prop('checked', storageValues.enabled);
+chrome.storage.sync.get('settings', function(storageValues) {
+  var settings = storageValues.settings;
+  if (settings === undefined)
+    settings = STEALTH_YOUTUBE_GLOBAL.defaultSettings;
+
+  $('#enabled').prop('checked', settings.enabled);
 
   $('#enabled').change(function() {
-    var isChecked = $(this).is(':checked');
-    chrome.storage.sync.set({enabled: isChecked}, function() {
+    settings.enabled = $(this).is(':checked');
+    chrome.storage.sync.set({settings: settings}, function() {
       alert('Settings saved.');
     });
   });
